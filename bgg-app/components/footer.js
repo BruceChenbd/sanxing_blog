089d40c67e1a7421fa3a553ac7f201a4eb148910
unknown <cbd@kgtdata.com>
2020-11-27 15:12:52 +0800

    提交最新

---
+++ bgg-app/components/footer.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const Footer = () => {
+    return  <footer className="footer">
+        <p>本站由 Next.js + express 驱动</p>
+        <p>©COPYRIGHT 2020 三省博客博主</p>
+    </footer>
+}
+
+
+export default Footer
